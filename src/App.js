48f6fa61aab7880f55abb058fc3e082b71b44324
Yafet Zerihun <zerihunyafet80@gmail.com>
2023-12-11 22:17:14 +0300

    handle error

--- src/App.js
+++ src/App.js
@@ -71,8 +71,7 @@ export default function App() {
           throw new Error("something went wrong with fetching movies");
         const data = await res.json();
 
-        if (data.Response === "False")
-          throw new Error("Movie not found in the database");
+        if (data.Response === "False") throw new Error("Movie not found");
         console.log(data);
         setMovies(data.Search);
         setIsLoading(false);
