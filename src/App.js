6f39e8bb0439208b4330782bd39b970140c0d930
Yafet Zerihun <zerihunyafet80@gmail.com>
2023-12-24 00:08:31 +0300

    adding keyboard to interact with the application

--- src/App.js
+++ src/App.js
@@ -70,6 +70,15 @@ export default function App() {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
 
+  useEffect(function () {
+    document.addEventListener("keydown", function (e) {
+      if (e.code === "Escape") {
+        handleCloseMovie();
+        console.log("closing");
+      }
+    });
+  }, []);
+
   useEffect(
     function () {
       const controller = new AbortController();
