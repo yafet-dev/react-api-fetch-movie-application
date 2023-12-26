e35778a01b12a282b2ca85463547d20baab724a5
Yafet Zerihun <zerihunyafet80@gmail.com>
2023-12-27 00:54:04 +0300

    focus on input field on key enter

--- src/App.js
+++ src/App.js
@@ -211,7 +211,14 @@ function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
   useEffect(function () {
-    inputEl.current.focus();
+    function callback(e) {
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
   }, []);
 
   return (
